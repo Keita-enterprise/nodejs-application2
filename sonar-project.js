769303c8a2a70c413d977373e89fd3a725dc9348
i2bkeit <keitaibrehima22@gmail.com>
2023-01-01 21:21:44 -0500

    sonarqube script added, Dockerfile added app.js and index.html added

---
+++ sonar-project.js
@@ -0,0 +1,18 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://54.197.197.67:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
+	    'sonar.login': 'i2bk',
+	    'sonar.password': 'i2bk',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
